c496cc537f8a20f73e20fab7c3c02e0a72b47c64
Peter Koscelansky <koscelansky@live.com>
2022-10-17 16:51:27 +0200

    added notes plugin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Reveal from 'reveal.js'
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js'
 import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js'
+import Notes from 'reveal.js/plugin/notes/notes.esm.js'
 
 import 'reveal.js/dist/reset.css'
 import 'reveal.js/dist/reveal.css'
@@ -8,7 +9,7 @@ import 'reveal.js/dist/theme/night.css'
 import 'reveal.js/plugin/highlight/zenburn.css'
 
 let deck = new Reveal({
-   plugins: [ Markdown, Highlight  ],
+   plugins: [ Markdown, Highlight, Notes ],
    slideNumber: 'c/t',
    hash: true,
    pdfSeparateFragments: false,
